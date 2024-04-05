21171935610b77e79d06e5772cd20111b9101796
이종우 <ghjkl133120@naver.com>
2024-04-05 17:04:32 +0900

    다른 장소에서 작업을 위한 commit

--- src/main/java/com/beaksoo/shop/ItemController.java
+++ src/main/java/com/beaksoo/shop/ItemController.java
@@ -1,14 +1,28 @@
 package com.beaksoo.shop;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
     @GetMapping("/list")
     String list(Model model){
-        model.addAttribute("name", "홍길동");
+        List<Item> result = itemRepository.findAll();
+        System.out.println(result.get(0).price);
+
+
+
+        model.addAttribute("name", "비싼 바지");
         return "list.html";
     }
 
--- src/main/java/com/beaksoo/shop/ShopApplication.java
+++ src/main/java/com/beaksoo/shop/ShopApplication.java
@@ -9,16 +9,6 @@ public class ShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 
-		var test = new Friend("park");
-		System.out.println(test.name);
-	}
 
-}
-
-class Friend{
-	String name = "kim";
-	int age = 20;
-	Friend(String a){
-		this.name = a;
 	}
 }
