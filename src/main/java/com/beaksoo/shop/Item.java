9c66da62c5fb8f7d4a846857869529c9a8a1ae08
이종우 <ghjkl133120@naver.com>
2024-04-08 14:52:19 +0900

    feat: 상품추가 기능 페이지생성

--- src/main/java/com/beaksoo/shop/Item.java
+++ src/main/java/com/beaksoo/shop/Item.java
@@ -1,26 +1,32 @@
 package com.beaksoo.shop;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Date;
 
 @Entity
 @ToString
+@Getter
+@Setter
 public class Item {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
-    public String title;
-    public Integer price;
+    private String title;
+    private Integer price;
 
 
 }
-
 @Entity
 class Notice{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     public String 글제목;
-    public Date 날짜;
+    public String 날짜;
 }
+
+
 
--- src/main/java/com/beaksoo/shop/ItemController.java
+++ src/main/java/com/beaksoo/shop/ItemController.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,11 +19,26 @@ public class ItemController {
     String list(Model model){
         List<Item> result = itemRepository.findAll();
         model.addAttribute("items", result);
-        var a = new Item();
-        System.out.println(a);
 
         return "list.html";
     }
+
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(String title, Integer price){
+
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+
+        return "redirect:/list";
+    }
+
 
 }
 
