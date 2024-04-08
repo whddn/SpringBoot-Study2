da85ddaf0d6486e8a8415e988fd2f6966febcf9f
이종우 <ghjkl133120@naver.com>
2024-04-08 14:55:45 +0900

    docs: @ModelAttribute로 데이터 바로 object로 변환

--- src/main/java/com/beaksoo/shop/ItemController.java
+++ src/main/java/com/beaksoo/shop/ItemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,11 +30,9 @@ public class ItemController {
     }
 
     @PostMapping("/add")
-    String addPost(String title, Integer price){
+    String addPost(@ModelAttribute Item item){
 
-        Item item = new Item();
-        item.setTitle(title);
-        item.setPrice(price);
+        System.out.println(item);
         itemRepository.save(item);
 
         return "redirect:/list";
