be5263eba26ab51d26aace458dc2f11264277a65
이종우 <ghjkl133120@naver.com>
2024-04-12 13:58:56 +0900

    feat: 회원가입 페이지 구현

--- src/main/java/com/beaksoo/shop/member/MemberController.java
+++ src/main/java/com/beaksoo/shop/member/MemberController.java
@@ -1,2 +1,13 @@
-package com.beaksoo.shop.member;public class MemberController {
+package com.beaksoo.shop.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("/register")
+    String register(){
+        return "register.html";
+    }
 }
