bf109b9f0e9216148e778c1170279bfb4256e645
이종우 <ghjkl133120@naver.com>
2024-04-12 15:04:50 +0900

    feat: 가입기능 만들기

--- src/main/java/com/beaksoo/shop/member/MemberController.java
+++ src/main/java/com/beaksoo/shop/member/MemberController.java
@@ -1,13 +1,37 @@
 package com.beaksoo.shop.member;
 
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.BCException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
+
+    private final MemberRepository memberRepository;
 
     @GetMapping("/register")
     String register(){
         return "register.html";
     }
+
+    @PostMapping("/member")
+    String addMember(
+            String username,
+            String password,
+            String displayName) {
+
+       Member member = new Member();
+       member.setUsername(username);
+       var hash = new BCryptPasswordEncoder().encode(password);
+       member.setPassword(hash);
+       member.setDisplayName(displayName);
+        memberRepository.save(member);
+        return "redirect:/list";
+    }
+
+
 }
---
+++ src/main/java/com/beaksoo/shop/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.beaksoo.shop.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
